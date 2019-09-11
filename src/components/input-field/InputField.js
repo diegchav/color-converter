099c111b07260f930581eb033813484a15445ee7
diegchav <diegchav.me@gmail.com>
2019-09-11 10:54:51 -0500

    Add propTypes to InputField component

--- src/components/input-field/InputField.js
+++ src/components/input-field/InputField.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './InputField.scss';
 
 const InputField = ({ value, onChangeValue }) => (
   <input type="text" value={value} onChange={onChangeValue} />
 );
+
+InputField.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChangeValue: PropTypes.func.isRequired
+};
 
 export default InputField;
